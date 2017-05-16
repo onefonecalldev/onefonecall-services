e35b2ba325c3902ebefed8e32c254f269704020f
onefonecalldev <onefonecalldev@gmail.com>
2017-05-16 16:19:44 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://pm.vaspp.com:82/onefonecall_services/';
+$config['base_url'] = 'http://onefonecall-services/';
 
 /*
 |--------------------------------------------------------------------------
